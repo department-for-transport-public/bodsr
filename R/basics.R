8d452bb3a96f462f6f3a4b68d7c22e9f6c78d0c1
Fran <francesca.bryden@dft.gov.uk>
2022-08-09 17:01:42 +0100

    basic function for calling timetable metadata

--- R/basics.R
+++ R/basics.R
@@ -1,15 +1,21 @@
-library(jsonlite)
-library(httr)
+##Function to pull in metadata
+get_timetable_metadata <- function(api_key, limit = 25) {
 
-url <- "https://data.bus-data.dft.gov.uk/api/v1/dataset?limit=25&search=Stagecoach&status=published&api_key=KEY HERE"
+  url <- paste0("https://data.bus-data.dft.gov.uk/api/v1/dataset?limit=",
+                limit,
+                "&api_key=",
+                api_key)
 
-download <- httr::GET(url)
+  download <- httr::GET(url)
 
 
-##Return err message if one or more atco codes are invalid
-if(http_status(download)$category != "Success"){
-  stop("An atco area code provided does not exist. Please check your query.
-    To display all valid atco codes, run lookup_atco_codes()")
+  ##Return err message if one or more atco codes are invalid
+ # if(http_status(download)
+
+  data <- jsonlite::fromJSON(
+    httr::content(download, as = "text", encoding = "UTF-8"))$results
+
+  return(data)
 }
 
-data <- fromJSON(content(download, as = "text", encoding = "UTF-8"))$results
+
