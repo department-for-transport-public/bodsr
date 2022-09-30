3ee6b9d6cc1edc223157be0c82afa6930f84e286
Fran <francesca.bryden@dft.gov.uk>
2022-09-30 09:16:53 +0100

    Added utility function to look up operator codes

---
+++ R/noc_lookup.R
@@ -0,0 +1,19 @@
+#' @name noc_lookup
+#' @title Lookup between operator names and national operator code (NOC) lookup
+
+noc_lookup <- function(){
+
+  ##Create temporary zip folder locations
+  tmp <- tempfile(fileext = ".zip")
+  tmp_csv <- tempdir()
+
+  #Download zip to location
+  download.file("https://data.bus-data.dft.gov.uk/catalogue/", tmp)
+
+  ##Extract only NOC lookup to another temp location
+  unzip(tmp, files = "operator_noc_data_catalogue.csv", exdir = tmp_csv)
+
+  ##Read in data
+  read.csv(file.path(tmp_csv, "operator_noc_data_catalogue.csv"))
+
+}
