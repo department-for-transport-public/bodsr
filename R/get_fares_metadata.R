bd6bbe67965275178afc946e0900341391ba4c93
Fran <francesca.bryden@dft.gov.uk>
2022-09-30 09:03:28 +0100

    Added fares API search

---
+++ R/get_fares_metadata.R
@@ -0,0 +1,65 @@
+##Get return from fares API
+
+#' @name get_fares_metadata
+#' @title Return fares metadata from the BODS API
+#' @export
+#'
+#' @param api_key API key for the BODS dataset passed as a string. Can be obtained from \link(https://data.bus-data.dft.gov.uk/api/)
+#' @param limit integer. Maximum number of records to return for a query. Defaults to 25
+#' @param search string to search records on; can be a value or partial value to
+#' match the data set name, data set description, organisation name, or admin
+#' area name.
+#'
+#' @importFrom httr GET content http_status
+#' @importFrom jsonlite fromJSON
+#'
+#' @return Returns a data frame of fares metadata including links to data from the BODS API.
+
+#Function to pull in metadata
+get_timetable_metadata <- function(api_key = Sys.getenv("BODS_KEY"),
+                                   limit = 25,
+                                   search = NULL) {
+
+  ##Check data values received
+  if(!is.numeric(limit)){
+    stop("Please provide an integer value to the limit argument")
+  }
+
+  ##Use search string if it's not null
+  if(!is.null(search)) {
+
+    ##Swap spaces for character
+    search <- gsub(" ", "%20", search)
+
+    search <- paste0("&search=", search)
+
+  } else {
+    search <- ""
+  }
+  #Paste together URL for API
+  url <- paste0("https://data.bus-data.dft.gov.uk/api/v1/fares/dataset?limit=",
+                limit,
+                search,
+                "&api_key=",
+                api_key)
+
+  #Raw content from api
+  download <- httr::GET(url)
+
+  ##Return error message if authentication failed
+  if(httr::http_status(download)$reason == "Unathorized"){
+    stop("Authentication credentials are not valid; please check you are using a valid BODS API key")
+  }
+
+  if(httr::http_status(download)$reason == "Bad Request"){
+    stop("Bad request; please check you have passed arguments to the function correctly")
+  }
+
+  data <- jsonlite::fromJSON(
+    httr::content(download, as = "text", encoding = "UTF-8"))$results
+
+  message(paste("Returning", nrow(data), "records"))
+
+  return(data)
+}
+
