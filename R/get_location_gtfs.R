fb950fe6e89cc9d6a96aa0b9279a83419b6f8dcc
Fran <francesca.bryden@dft.gov.uk>
2022-10-20 08:55:19 +0100

    Added function to get GTFS data

---
+++ R/get_location_gtfs.R
@@ -0,0 +1,73 @@
+##Return vehicle location data in GTFS-RT format from the BODS API
+
+#' @name get_location_gtfs
+#' @title Return GTFS-RT location data from the BODS API
+#' @export
+#'
+#' @param api_key API key for the BODS dataset passed as a string. Can be obtained from \link(https://data.bus-data.dft.gov.uk/api/)
+#' @param bounding_box vector of four numerics. Limit results to location data
+#' for vehicles within the rectangular boundingBox you set using co-ordinates
+#' [minLatitude, maxLatitude, minLongitude, maxLongitude].
+#' Defaults to NULL.
+#' @param route_id string or vector of strings. Limit results to bus location data with the specified routeId.
+#' Defaults to NULL.
+#' @param start_time_after integer. Limit results to bus location data with a
+#' start time after the specified Unix timestamp. Defaults to NULL.
+#' @param start_time_before integer. Limit results to bus location data with a
+#' start time before the specified Unix timestamp. Defaults to NULL.
+#'
+#' @importFrom httr GET content http_status
+#'
+#' @return Returns bus location data in GTFS-RT format. More detail on this format can be found \link(https://data.bus-data.dft.gov.uk/guidance/requirements/?section=dataformats)
+
+#Function to pull in metadata
+get_location_gtfs <- function(api_key = Sys.getenv("BODS_KEY"),
+                             bounding_box = NULL,
+                             route_id = NULL,
+                             start_time_after = NULL,
+                             start_time_before = NULL) {
+
+  ##Use bounding box coordinates to search on
+  if(!is.null(bounding_box)){
+
+    ##If there's not 4 coordinates, stop
+    if(length(bounding_box) != 4){
+      stop("Incorrect number of coordinates provided to bounding_box argument")
+    }
+
+    bounding_box <- paste0("boundingBox=",
+                           paste0(bounding_box, collapse = ","),
+                           "&")
+  }
+
+  ##Create search strings for arguments if they are not null
+  route_id <- not_null(route_id, "routeId")
+  start_time_after <- not_null(start_time_after, "startTimeAfter")
+  start_time_before <- not_null(start_time_before, "startTimeBefore")
+
+
+  #Paste together URL for API
+  url <- paste0("https://data.bus-data.dft.gov.uk/api/v1/gtfsrtdatafeed/?",
+                bounding_box,
+                route_id,
+                start_time_after,
+                start_time_before,
+                "api_key=", api_key)
+
+  ##Read from url
+  download <- httr::GET(url)
+
+
+  ##Return error message if authentication failed
+  if(httr::http_status(download)$reason == "Unathorized"){
+    stop("Authentication credentials are not valid; please check you are using a valid BODS API key")
+  }
+
+  if(httr::http_status(download)$reason == "Bad Request"){
+    stop("Bad request; please check you have passed arguments to the function correctly")
+  }
+
+  ##Return GTFS-RT data
+  return(download)
+
+}
