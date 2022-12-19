36edcdf5b0e2a94455b1606f6aa129baabbf5057
Fran <francesca.bryden@dft.gov.uk>
2022-12-19 14:30:25 +0000

    Move all xml reading together

--- R/get_timetable_data.R
+++ R/get_timetable_data.R
@@ -1,3 +1,44 @@
+
+##List everything inside a remote zip file
+zip_list <- function(url){
+
+  ##Download to temp location
+  folder <- tempfile()
+
+  httr::GET(
+    url = url,
+    write_disk(folder, overwrite = TRUE)
+  )
+
+  ##Read in first file as an XML
+  files <- utils::unzip(folder, list = TRUE)
+
+  return(files)
+}
+
+##Download and read xml
+get_raw_xml <- function(url){
+
+  xml_loc <- tempfile(fileext = ".xml")
+
+  httr::GET(
+    url = url,
+    write_disk(xml_loc, overwrite = TRUE)
+  )
+
+  xml2::read_xml(xml_loc)
+
+}
+
+##Return a specific value from xml
+find_node_value <- function(x, xpath){
+
+  xml2::xml_find_all(x = x, xpath = xpath) %>%
+    xml2::as_list() %>%
+    unlist()
+}
+
+
 ##Get line-level details from an xml file
 line_level_xml <- function(xml){
 
@@ -17,6 +58,10 @@ line_level_xml <- function(xml){
 
 meta <- bodsr::get_timetable_metadata()
 
+
+##Try to unzip with names
+if(meta$extension == "xml"){
+
 ##Extract data from xml file for a single line
 extract_single_line <- function(x){
 
@@ -25,14 +70,10 @@ extract_single_line <- function(x){
     dplyr::select(url, dataSetID = id,
                   operatorName, description, status, extension, dqScore, dqRag)
 
-  ##Try to unzip with names
-  if(meta$extension == "xml"){
-
     ##Read in the xml
     get_raw_xml(meta$url) %>%
     ##Get line-level detail for it
       line_level_xml() %>%
       ##Join to the URL deets
       bind_cols(meta)
-  }
 }
